3b43c15c3a75cef4d023640359794c9a194dc213
AndreC <amcarrin@gmail.com>
2022-08-19 22:15:03 -0400

    Added sPA

--- src/deeproc/DeepROC.py
+++ src/deeproc/DeepROC.py
@@ -100,6 +100,7 @@ class DeepROC(FullROC):
         from deeproc.Helpers.DeepROCFunctions import concordant_partial_AUC
         from deeproc.Helpers.DeepROCFunctions import discrete_partial_roc_measures
         from deeproc.Helpers.DeepROCFunctions import partial_area_index_proxy
+        from deeproc.Helpers.DeepROCFunctions import standardized_partial_area_proxy
         from deeproc.Helpers.DeepROCFunctions import areEpsilonEqual
         from deeproc.Helpers.DeepROCFunctions import showConcordanceMatrixInfo
         from deeproc.Helpers.DeepROCFunctions import showCmeasures
@@ -249,6 +250,12 @@ class DeepROC(FullROC):
         if not quiet:
             showAllMeasures = True
             showDiscretePartialAUCmeasures(groupIndex+1, measure_dict, showAllMeasures, verbose=verbose)
+        #endif
+
+        sPA = standardized_partial_area_proxy(partial_fpr, partial_fpr, quiet)
+        measure_dict.update(dict(sPA=sPA))
+        if not quiet and verbose:
+            print(f"{'sPA':16s} = {sPA:0.4f}")
         #endif
 
         # show PAI result only if the group ends at FPR == 0
